dcdf00c3871191e6f47f00729ae2f872c0312817
irving-frias <irvingfrias41@gmail.com>
2024-04-01 15:08:48 -0700

    Added changes

--- js-src/language-toggle.js
+++ js-src/language-toggle.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Get the current language
   const currentLang = document.querySelector('html').getAttribute('lang');
+  const currentPath = currentLang == 'en' ? document.querySelector('html').getAttribute('data-slug-es') : document.querySelector('html').getAttribute('data-slug-en');
 
   // Get the language toggle button
   const languageToggle = document.querySelector('a.language-toggle');
@@ -13,8 +14,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const currentLink = document.querySelector('a[href="' + currentUrl + '"]');
 
-  let url_translation = currentLink.dataset.translateUrl;
-
   if (currentLink) {
     headerLinks.querySelectorAll('a').forEach(link => {
       link.classList.remove('active');
@@ -25,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
     currentLink.classList.add('active');
-
-    languageToggle.setAttribute('href', url_translation);
   }
+
+  languageToggle.setAttribute('href', currentPath);
 });
